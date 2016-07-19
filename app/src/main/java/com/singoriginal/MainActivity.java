79efd1c4d38e3271d5b5166cce3f3400731757c7
liruya <1046074984@qq.com>
2016-07-19 21:03:39 +0800

    2016.07.19 1. 页面"我的"布局

--- app/src/main/java/com/singoriginal/MainActivity.java
+++ app/src/main/java/com/singoriginal/MainActivity.java
@@ -1,15 +1,28 @@
 package com.singoriginal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity
+import com.singoriginal.fragment.MusicFragment;
+
+public class MainActivity extends FragmentActivity
 {
-
+    private FrameLayout main_fl_show;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView()
+    {
+        main_fl_show = (FrameLayout) findViewById(R.id.main_fl_show);
+        MusicFragment musicFrag = new MusicFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_fl_show, musicFrag).commit();
+//        main_fl_show.addView(new MusicFragment());
     }
 }
---
+++ app/src/main/java/com/singoriginal/constant/ConstVal.java
@@ -0,0 +1,11 @@
+package com.singoriginal.constant;
+
+/**
+ * Created by lanouhn on 16/7/19.
+ */
+public class ConstVal
+{
+    //颜色值 ARGB格式
+    public static final int colorDKGreen = 0xFF00BB9C;
+    public static final int colorHyaline = 0x00FFFFFF;
+}
