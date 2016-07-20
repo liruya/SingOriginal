e1e2deed874629ee70ae7f66f4cee4031e4cde23
lanouhn <1411949462@qq.com>
2016-07-20 20:18:31 +0800

    2016.7.20 频道标题 和 动态标题完成

--- app/src/main/java/com/singoriginal/fragment/DynamicSquareFragment.java
+++ app/src/main/java/com/singoriginal/fragment/DynamicSquareFragment.java
@@ -2,11 +2,17 @@ package com.singoriginal.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.singoriginal.R;
+import com.singoriginal.constant.CommanVal;
+import com.singoriginal.constant.ConstVal;
 
 /**
  * Created by lanouhn on 16/7/19.
@@ -20,7 +26,46 @@ public class DynamicSquareFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dynamic_square, container, false);
+        View view = inflater.inflate(R.layout.fragment_dynamic_square, null);
+
+        initView(view);
+        return view;
     }
+
+    private void initView(View view) {
+
+        //如果已登录则显示个人信息页面,否则显示登录注册页面 标题头及页面主体均不相同
+        //页面公用标题头初始化
+        View incView = view.findViewById(R.id.dynamic_include_header);
+
+        ImageButton image = (ImageButton) incView.findViewById(R.id.hdr_ib_srch);
+        image.setImageResource(R.mipmap.find_dynamic_send_prs);
+
+        RadioButton tv_first = (RadioButton) incView.findViewById(R.id.hdr_rb_first);
+        tv_first.setText(getString(R.string.dynamic));
+        tv_first.setChecked(true);
+
+        incView.findViewById(R.id.hdr_rb_second).setVisibility(View.GONE);
+
+        RadioButton tv_third = (RadioButton) incView.findViewById(R.id.hdr_rb_third);
+        tv_third.setText(getString(R.string.square));
+
+        int color = CommanVal.isLogin ? ConstVal.colorHyaline : ConstVal.colorDKGreen;
+        incView.setBackgroundColor(color);
+        Toast.makeText(getContext(), CommanVal.isLogin + "", Toast.LENGTH_SHORT).show();
+        //"频道"主体页面
+        FragmentTransaction beginTransaction = getActivity().getSupportFragmentManager()
+                .beginTransaction();
+        Fragment frag;
+        if (CommanVal.isLogin) {
+            frag = new MyinfoFragment();
+        } else {
+            frag = new NotloginFragment();
+        }
+        beginTransaction.replace(R.id.dynamic_frameLayout, frag).commit();
+    }
+
 }
--- app/src/main/java/com/singoriginal/fragment/MyFragment.java
+++ app/src/main/java/com/singoriginal/fragment/MyFragment.java
@@ -17,13 +17,11 @@ import com.singoriginal.constant.ConstVal;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MyFragment extends Fragment
-{
+public class MyFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+                             Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_my, null);
         initView(view);
@@ -32,32 +30,29 @@ public class MyFragment extends Fragment
 
     /**
      * "我的"页面视图初始化
+     *
      * @param view
      */
-    private void initView(View view)
-    {
+    private void initView(View view) {
         //如果已登录则显示个人信息页面,否则显示登录注册页面 标题头及页面主体均不相同
         //页面公用标题头初始化
         View incView = view.findViewById(R.id.my_inc_hdr);
-        incView.findViewById(R.id.hdr_ib_srch).setVisibility(View.GONE);
-        incView.findViewById(R.id.hdr_rb_second).setVisibility(View.GONE);
-        RadioButton tv_second = (RadioButton) incView.findViewById(R.id.hdr_rb_first);
+        incView.findViewById(R.id.hdr_ib_srch).setVisibility(View.INVISIBLE);
+        incView.findViewById(R.id.hdr_rb_first).setVisibility(View.GONE);
+        RadioButton tv_second = (RadioButton) incView.findViewById(R.id.hdr_rb_second);
         tv_second.setText(getString(R.string.me));
         tv_second.setChecked(true);
         incView.findViewById(R.id.hdr_rb_third).setVisibility(View.GONE);
         int color = CommanVal.isLogin ? ConstVal.colorHyaline : ConstVal.colorDKGreen;
         incView.setBackgroundColor(color);
-        Toast.makeText(getContext(), CommanVal.isLogin+"", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), CommanVal.isLogin + "", Toast.LENGTH_SHORT).show();
         //"我的"主体页面
         FragmentTransaction beginTransaction = getActivity().getSupportFragmentManager()
-                                                               .beginTransaction();
+                .beginTransaction();
         Fragment frag;
-        if (CommanVal.isLogin)
-        {
+        if (CommanVal.isLogin) {
             frag = new MyinfoFragment();
-        }
-        else
-        {
+        } else {
             frag = new NotloginFragment();
         }
         beginTransaction.replace(R.id.my_fl_show, frag).commit();
